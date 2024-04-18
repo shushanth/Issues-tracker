d2f4265ea7d9cbac9d5ac01aad065f12190c6d8d
shushanth <shushant3112@gmail.com>
2024-04-18 22:06:45 +0200

    Handled auth token interceptors

--- issues-service/src/interceptors/token.interceptor.ts
+++ issues-service/src/interceptors/token.interceptor.ts
@@ -5,16 +5,21 @@ import {
   ExecutionContext,
   CallHandler,
   UnauthorizedException,
+  Logger,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-
 @Injectable()
 export class TokenInterceptor implements NestInterceptor {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private logger: Logger,
+    private jwtService: JwtService,
+  ) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    this.logger.log(token, '<-------token---->');
+    this.logger.log(type, '<-----token type---->');
     if (type === 'Bearer' && token) {
       try {
         const payload = this.jwtService.verifyAsync(token, {
